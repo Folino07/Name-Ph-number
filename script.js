f5b1502acf72ee80c2a15df5bd01c316fa4a7e1b
Folino07 <98807100+Folino07@users.noreply.github.com>
2022-02-03 08:14:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var Name = prompt ("Enter name");
+ 
+var TelephoneNumber= prompt("Enter phone number");
+
+var mensajefinal = "Name:" + Name + ".TelephoneNumber:"+ TelephoneNumber
+
+    alert(mensajefinal);
+
+
